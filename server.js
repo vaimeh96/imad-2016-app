24ccbdd0d08afedb021732ecf69ed579de089dc6
vaimeh96 <vaibhav_tushar@srmuniv.edu.in>
2016-09-23 09:00:38 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/article-one', function(req,res){ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));});
 app.get('/article-two', function(req,res){ res.send('Article two requesed and will be served here');});
 app.get('/article-three', function(req,res){ res.send('Article three requesed and will be served here');});
 app.get('/ui/style.css', function (req, res) {
