584a250399616dca97aa550208072ae3a8cf9550
vaimeh96 <vaibhav_tushar@srmuniv.edu.in>
2016-09-23 10:16:48 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,8 +61,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req,res){ 
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));});
-app.get('/article-two', function(req,res){ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'))});
-app.get('/article-three', function(req,res){ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'))});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
