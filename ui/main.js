25dd12c92e24a9ca829154d77c9ef39d58a6f40e
vaimeh96 <vaibhav_tushar@srmuniv.edu.in>
2016-09-23 22:16:31 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 var img=getElementById('madi');
 img.onclick = function(){
-    img.style.marginleft='100px';
+    img.style.marginLeft='100px';
 };
