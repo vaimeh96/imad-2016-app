ee2dac90dad23bac673cc10c2ecd57353a2c20bc
vaimeh96 <vaibhav_tushar@srmuniv.edu.in>
2016-09-23 21:57:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,13 +60,14 @@ app.get('/:articleName', function(req,res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
