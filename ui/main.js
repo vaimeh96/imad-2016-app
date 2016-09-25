a927c034f8e2dcf59e2b6a25a6bdd6f8c632dfac
vaimeh96 <vaibhav_tushar@srmuniv.edu.in>
2016-09-25 08:42:50 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,21 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-
-var img=getElementById('madi');
-img.onclick = function(){
-    img.style.marginLeft='100px';
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+var request = new XMLhttpRequest();
+request.onreadystatechange=function(){
+    if(request.readystate===XMLhttpRequest.done){
+        if(request.status===200)
+        {
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+    }
 };
+request.open('GET','http://vaimeh96.imad.hasura-app.io/counter',true);
+request.send(null);
+
+
+    
+}
